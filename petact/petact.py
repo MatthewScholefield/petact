e7a2bbd5db672186715f3cc37d02ea9c29a60362
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-23 15:44:27 +0000

    Adding tarfile member sanitization to extractall()

--- petact/petact.py
+++ petact/petact.py
@@ -80,7 +80,26 @@ def download_extract_tar(tar_url, folder, tar_filename=''):
         download(tar_url, data_file)
 
     with tarfile.open(data_file) as tar:
-        tar.extractall(path=folder)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, path=folder)
 
 
 def install_package(tar_url, folder, md5_url='{tar_url}.md5',
